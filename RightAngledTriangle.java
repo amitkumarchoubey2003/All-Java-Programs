b4e7ae8f18f10ad163ba1c0ec028679720217b6d
AMIT KUMAR CHOUBEY <162465029+amitkumarchoubey2003@users.noreply.github.com>
2024-07-31 22:37:31 +0530

    Right Angled Triangle Pattern

---
+++ RightAngledTriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RightAngledTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
