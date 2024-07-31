f2e5d3746b2ca8b69d9596e33232b9c8577e0161
AMIT KUMAR CHOUBEY <162465029+amitkumarchoubey2003@users.noreply.github.com>
2024-07-31 21:48:45 +0530

    Sum Of Natural Number

--- SumOfNaturalNumber.java
+++ SumOfNaturalNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfNaturalNumbers {
+public class SumOfNaturalNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a limit: ");
