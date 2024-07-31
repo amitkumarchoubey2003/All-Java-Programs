c3a14ef29080151be92f55290334fb0dbc90e8e0
AMIT KUMAR CHOUBEY <162465029+amitkumarchoubey2003@users.noreply.github.com>
2024-07-31 21:35:02 +0530

    SumOfNaturalNumbers.java

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a limit: ");
+        int limit = scanner.nextInt();
+        scanner.close();
+
+        int sum = 0;
+
+        for (int i = 1; i <= limit; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of natural numbers up to " + limit + ": " + sum);
+    }
+}
