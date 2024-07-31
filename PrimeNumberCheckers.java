350675f3f88b2afc5dbb29a00634cbacb7a48655
AMIT KUMAR CHOUBEY <162465029+amitkumarchoubey2003@users.noreply.github.com>
2024-07-31 21:45:20 +0530

    Prime Number Checkers

--- PrimeNumberCheckers.java
+++ PrimeNumberCheckers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrimeNumberChecker {
+public class PrimeNumberCheckers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
