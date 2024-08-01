36bf734ce160d321c43a4be91e9791980d70c3b1
AMIT KUMAR CHOUBEY <162465029+amitkumarchoubey2003@users.noreply.github.com>
2024-08-01 13:35:29 +0530

    Pascals Triangle

---
+++ PascalsTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(pascal(i, j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int pascal(int i, int j) {
+        if (j == 0 || j == i) {
+            return 1;
+        } else {
+            return pascal(i - 1, j - 1) + pascal(i - 1, j);
+        }
+    }
+}
